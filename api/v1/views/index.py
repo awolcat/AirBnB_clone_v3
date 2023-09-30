c65793c60ea35b8400e33e98f0f596cf6c4f768e
Albert <albertirura11@gmail.com>
2023-09-30 12:44:48 +0300

    added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,28 @@
 """This module defines the blueprint app_views"""
 from flask import make_response
 from api.v1.views import app_views
+from models import storage
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
 
 
+classes = {'users': User, 'states': State, 'reviews': Review, 'places': Place,
+        'cities': City, 'amenities': Amenity}
 @app_views.route('/status')
 def api_status():
     """Return a JSON"""
     payload = {'status':'OK'}
     return make_response(payload)
+
+
+@app_views.route('/stats')
+def statistics():
+    """Return object stats"""
+    temp = {}
+    for key, value in classes.items():
+        temp[key] = storage.count(cls=value)
+    return make_response(temp)
