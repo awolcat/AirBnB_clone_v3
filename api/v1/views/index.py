dda7618f468e1b72720d8bcf89b4e3cf3a982c8c
Albert <albertirura11@gmail.com>
2023-09-30 20:03:08 +0300

    fix bug

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,14 @@ from flask import make_response
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def api_status():
     """Return a JSON"""
     payload = {'status': 'OK'}
     return make_response(payload)
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def statistics():
     """Return object stats"""
     from models import storage
