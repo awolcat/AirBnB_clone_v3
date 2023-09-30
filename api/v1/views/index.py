6b42734fa9753c8dc00c922ff09022263d109480
Albert <albertirura11@gmail.com>
2023-09-30 13:13:42 +0300

    bug?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,22 +9,3 @@ def api_status():
     """Return a JSON"""
     payload = {'status': 'OK'}
     return make_response(payload)
-
-
-@app_views.route('/stats')
-def statistics():
-    """Return object stats"""
-    from models import storage
-    from models.user import User
-    from models.state import State
-    from models.review import Review
-    from models.place import Place
-    from models.city import City
-    from models.amenity import Amenity
-
-    classes = {'users': User, 'states': State, 'reviews': Review,
-               'places': Place, 'cities': City, 'amenities': Amenity}
-    temp = {}
-    for key, value in classes.items():
-        temp[key] = storage.count(cls=value)
-    return make_response(temp)
