684712ce8888f3652863e119766435062315b708
Albert <albertirura11@gmail.com>
2023-09-30 11:45:22 +0300

    added status api route

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines the blueprint app_views"""
+from flask import make_response
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def api_status():
+    """Return a JSON"""
+    payload = {'status':'OK'}
+    return make_response(payload)
