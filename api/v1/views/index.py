b22144b9c5f5d967e2257dee70755cb6b694fe55
Albert <albertirura11@gmail.com>
2023-09-30 13:10:52 +0300

    bug fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,17 +2,6 @@
 """This module defines the blueprint app_views"""
 from flask import make_response
 from api.v1.views import app_views
-from models import storage
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-
-
-classes = {'users': User, 'states': State, 'reviews': Review, 'places': Place,
-           'cities': City, 'amenities': Amenity}
 
 
 @app_views.route('/status')
@@ -25,6 +14,16 @@ def api_status():
 @app_views.route('/stats')
 def statistics():
     """Return object stats"""
+    from models import storage
+    from models.user import User
+    from models.state import State
+    from models.review import Review
+    from models.place import Place
+    from models.city import City
+    from models.amenity import Amenity
+
+    classes = {'users': User, 'states': State, 'reviews': Review,
+               'places': Place, 'cities': City, 'amenities': Amenity}
     temp = {}
     for key, value in classes.items():
         temp[key] = storage.count(cls=value)
